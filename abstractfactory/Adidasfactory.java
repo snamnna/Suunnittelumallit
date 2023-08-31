cd90095cea1725523f277201f13fad33e1e20a58
sanna <sanna.lohkovuori@hotmail.com>
2023-08-31 13:01:05 +0300

    Tehty classit vaatteille ja adidasfactory

---
+++ abstractfactory/Adidasfactory.java
@@ -0,0 +1,30 @@
+package abstractfactory;
+
+public class Adidasfactory implements AbstractFactory{
+
+    @Override
+    public Jeans createJeans() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'createJeans'");
+    }
+
+    @Override
+    public Tshirt createTshirt() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'createTshirt'");
+    }
+
+    @Override
+    public Cap createCap() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'createCap'");
+    }
+
+    @Override
+    public Shoes createShoes() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'createShoes'");
+    }
+    
+    
+}
